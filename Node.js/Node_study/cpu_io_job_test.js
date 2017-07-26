56e153c7eec6e631bdf1fc5007c487117bf58d3a
ParkYeoungJun <dudwns4971@gmail.com>
2017-07-26 20:21:16 +0900

    commit from labtop

--- Node.js/Node_study/cpu_io_job_test.js
+++ Node.js/Node_study/cpu_io_job_test.js
@@ -5,9 +5,7 @@ console.log('the third CPU Bound working');
 
 // I/O Job
 console.log('the first CPU Bound working');
-
 setTimeout(function(){
     console.log('the second CPU Bound working');
 }, 3000);
-
 console.log('the third CPU Bound working');
