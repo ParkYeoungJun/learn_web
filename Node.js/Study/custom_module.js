2204b94dcb77391c9375dd9eecc1257ab958409e
ParkYeoungJun <dudwns4971@gmail.com>
2017-08-02 20:05:48 +0900

    commit from labtop

---
+++ Node.js/Study/custom_module.js
@@ -0,0 +1,14 @@
+function add(a, b)
+{
+    return a + b;
+}
+
+var json = {
+    id : 'abc',
+    pw : '123'
+}
+
+module.exports = add;
+module.exports.json = json;
+// 그냥 exports도 됨, 상하위 관계가 있음
+// module.exports.m = json; 가능 호출할 대 custom.m.id
---
+++ Node.js/Study/module_test.js
@@ -0,0 +1,5 @@
+var custom = require('./custom_module');
+
+
+console.log('add : ' + custom(3,5));
+console.log('id : ' + custom.json.id + ' pw : ' + custom.json.pw );
